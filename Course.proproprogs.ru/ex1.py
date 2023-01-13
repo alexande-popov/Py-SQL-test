5a6782fa9a9386d183d5886a608d72319a6f8d75
Popov Alexander <alexande-popov@yandex.ru>
2023-01-13 21:38:28 +0300

    Feat[proproprogs.ru]: Fill table, select, print
    Examples of INSERT.
    
    Examples of SELECT.
    
    Print result of selection.

--- Course.proproprogs.ru/ex1.py
+++ Course.proproprogs.ru/ex1.py
@@ -13,4 +13,41 @@ with sq.connect("saper.db") as con:
         score INTEGER
     )""")
 
-    sql = "INSERT INTO users (name, old, score) VALUES('Алексей', 18, 1000)"
+    ## Fill the table
+    row1_sql = "INSERT INTO users VALUES(1, 'Михаил', 1, 19, 1000)"
+    cur.execute(row1_sql)
+
+    cur.execute("INSERT INTO users (name, sex, old, score) VALUES('Александр', 1, 18, 500)")
+    cur.execute("INSERT INTO users (name, old, score) VALUES('Алексей', 22, 200)")  # sex is DEFAULT
+    cur.execute("INSERT INTO users (name, sex, old, score) VALUES('Мария', 2, 30, 1200)")
+
+    rows56_sql = """
+        INSERT INTO users (name, old, score) VALUES
+            ('Александр', 12, 100),
+            ('Григорий', 57, 1500)
+    """
+    cur.execute(rows56_sql)
+
+    # 17 is not in order for increment, but ok.
+    # if 1, 2 or so on -- IntegrityError: UNIQUE constraint failed: users.user_id
+    row7_sql = "INSERT INTO users VALUES(17, 'Николай', 1, 35, 900)"
+    cur.execute(row7_sql)
+
+    # typing: old INTEGER = 'twenty'
+    cur.execute("INSERT INTO users (name, sex, old, score) VALUES('Ольга', 2, 'twenty', 1200)")
+
+    ## Select
+    select_sql = "SELECT * FROM users WHERE score >= 500 AND sex = 1 ORDER BY score ASC LIMIT 2, 3"
+    cur.execute(select_sql)
+
+    for result in cur:
+        print(result)
+
+    # result as list of tuples
+    result = cur.fetchall()
+    # print(result)
+
+    # # in order by one
+    # print(cur.fetchone(), '\n', cur.fetchone())
+
+
