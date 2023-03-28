ed202a8ddbfe07efbdbfa09cd561fb417dac6a61
Popov Alexander <alexande-popov@yandex.ru>
2023-03-29 00:24:19 +0300

    Update[bd_indexes]: Create and fill DB.

--- db_indexes/play_with.py
+++ db_indexes/play_with.py
@@ -2,7 +2,7 @@ import sqlite3 as sq
 import random
 
 
-def do_template(table_name, **kwargs):
+def do_insert_template(table_name, **kwargs):
     """Return string 'INSERT INTO table_name ({field1, ...) VALUES (val1, ...);'"""
 
     keys = [str(k) for k in kwargs.keys()]
@@ -11,15 +11,40 @@ def do_template(table_name, **kwargs):
     fields_names = ", ".join(keys)
     values = ", ".join(vals)
 
-    sql_insert_val_args = f"INSERT INTO {table_name} ({fields_names}) VALUES ({values});"
+    sql_insert_val_args = f"INSERT INTO {table_name} ({fields_names}) VALUES ({values})"
 
     return sql_insert_val_args
 
 
-s = do_template(table_name="t", a=1, b='2')
-print(s)
-for i in range(10):
-    val1 = random.randint(0, 1000)
-    val2 = random.randint(0, 1000)
-    s = do_template(table_name="t", Col1=val1, Col2=val2)
-    print(s)
+db_name = "my_db"
+with sq.connect(f"{db_name}.db") as con:
+    cur = con.cursor()
+
+    table_name = "t"
+    target_field = "col1"
+    index_name = target_field + "_inx"
+
+    # delete every time for rewriting to conveniently run script
+    cur.execute(f"DROP TABLE IF EXISTS {table_name}")
+    cur.execute(f"DROP INDEX IF EXISTS {index_name}")
+
+    cur.execute(f"""CREATE TABLE IF NOT EXISTS {table_name} (
+        {target_field} INTEGER,
+        col2 INTEGER
+    )""")
+    cur.execute(f"CREATE INDEX IF NOT EXISTS {index_name} ON {table_name}({target_field})")
+
+    ## Fill the table
+    cardinality = 100
+    for i in range(cardinality):
+
+        val1 = random.randint(0, 1000)
+        val2 = random.randint(0, 1000)
+
+        insert_sql = do_insert_template(table_name=table_name, col1=val1, col2=val2)
+        cur.execute(insert_sql)
+
+        cur.execute(f"SELECT * FROM {table_name}")
+
+        # for result in cur:
+        #     print(result)
